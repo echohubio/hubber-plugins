8e09b621ea54e85f24dc247e3d461eb778496246
John Ferlito <johnf@inodes.org>
2017-01-23 16:29:58 +1100

    Push plugin state

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+import path from 'path';
+import fs from 'fs';
 import Debug from 'debug';
 
 const debug = Debug('hubber:plugin:plugins');
 
-const storePluginState = (iot) => {
-  const plugins = {};
+const storePluginState = (iot, architect) => {
+  debug('syncing plugin state');
+
+  const plugins = [];
+
+  architect.config.forEach((pluginData) => {
+    const pluginPath = pluginData.packagePath;
+    if (!pluginPath) {
+      return;
+    }
+
+    const packagePath = path.join(pluginPath, 'package.json');
+    const metadata = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+    const plugin = {
+      name: metadata.name,
+      version: metadata.version,
+      description: metadata.description,
+      author: metadata.author,
+      license: metadata.license,
+      homepage: metadata.homepage,
+      bugs: metadata.bugs,
+    };
+    plugins.push(plugin);
+  });
+
+  debug(plugins);
   const state = {
     plugins,
   };
@@ -38,8 +64,8 @@ const setup = (options, imports, register) => {
     },
   });
 
-  hub.on('ready', () => {
-    storePluginState(iot);
+  hub.on('ready', (architect) => {
+    storePluginState(iot, architect);
   });
 };
 
